c32263e4212f88d11ff7e38c9c33b9c585e37d16
alan <alan@onebillion.org>
2016-06-21 17:06:04 +0100

    eod

--- app/src/main/java/org/onebillion/xprz/controls/OBTextLayer.java
+++ app/src/main/java/org/onebillion/xprz/controls/OBTextLayer.java
@@ -6,6 +6,12 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.style.ForegroundColorSpan;
 
 /**
  * Created by alan on 21/04/16.
@@ -19,6 +25,8 @@ public class OBTextLayer extends OBLayer
     public int colour;
     public Paint textPaint;
     float lineOffset;
+    int hiStartIdx=-1,hiEndIdx=-1;
+    int hiRangeColour;
 
 
     float letterSpacing;
@@ -50,7 +58,15 @@ public class OBTextLayer extends OBLayer
         return obj;
     }
 
+    public void drawHighText(Canvas canvas)
+    {
+        SpannableString ss = new SpannableString(text);
+        ss.setSpan(new ForegroundColorSpan(hiRangeColour),hiStartIdx,hiEndIdx, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        StaticLayout ly = new StaticLayout(ss,new TextPaint(textPaint),tempRect.width(), Layout.Alignment.ALIGN_NORMAL,0,0,false);
+    }
+
     @Override
+
     public void draw(Canvas canvas)
     {
         textPaint.setTextSize(textSize);
@@ -106,4 +122,10 @@ public class OBTextLayer extends OBLayer
         this.letterSpacing = letterSpacing;
     }
 
+    public void setHighRange(int st,int en,int colour)
+    {
+        hiStartIdx = st;
+        hiEndIdx = en;
+        hiRangeColour = colour;
+    }
 }
