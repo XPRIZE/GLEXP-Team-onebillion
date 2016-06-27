93845232ec97ae937dd2cbb108679c62e98a17f0
alan <alan@onebillion.org>
2016-06-27 16:24:50 +0100

    add OBPhoneme

---
+++ app/src/main/java/org/onebillion/xprz/utils/OBPhoneme.java
@@ -0,0 +1,16 @@
+package org.onebillion.xprz.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by alan on 27/06/16.
+ */
+public class OBPhoneme
+{
+    String text, audio, soundid;
+    List<List<Float>> timings = new ArrayList<>();
+    Map<String,Object> properties = new HashMap<>();
+}
