11bd72f200524e83faf2877d52bfe99f30909a75
alan <alan@onebillion.org>
2017-03-02 16:20:38 +0000

    create OC_Doodle

---
+++ app/src/main/java/org/onebillion/onecourse/mainui/oc_playzone/OC_Doodle.java
@@ -0,0 +1,11 @@
+package org.onebillion.onecourse.mainui.oc_playzone;
+
+import org.onebillion.onecourse.mainui.OC_SectionController;
+
+/**
+ * Created by alan on 02/03/2017.
+ */
+
+public class OC_Doodle extends OC_SectionController
+{
+}
