bb6f7a4feadc152fdfa324872bb4d519fb449d0c
alan <alan@onebillion.org>
2018-06-13 16:39:55 +0100

    Create OC_Morphology_A,OC_Morphology_3

---
+++ app/src/main/java/org/onebillion/onecourse/mainui/oc_morphology/OC_Morphology_3.java
@@ -0,0 +1,4 @@
+package org.onebillion.onecourse.mainui.oc_morphology;
+
+public class OC_Morphology_3 extends OC_Morphology {
+}
---
+++ app/src/main/java/org/onebillion/onecourse/mainui/oc_morphology/OC_Morphology_A.java
@@ -0,0 +1,4 @@
+package org.onebillion.onecourse.mainui.oc_morphology;
+
+public class OC_Morphology_A extends OC_Morphology {
+}
