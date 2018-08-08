50d27fef47836d70ceda01a14e62ea89a86121e8
KaMpErTuGa <pedro.pina.loureiro@gmail.com>
2018-08-08 15:17:50 +0100

    Converted Attributions to text view

--- app/src/main/java/org/onebillion/onecourse/mainui/OBSetupMenu_AttributionsView.java
+++ app/src/main/java/org/onebillion/onecourse/mainui/OBSetupMenu_AttributionsView.java
@@ -1,17 +1,23 @@
 package org.onebillion.onecourse.mainui;
 
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Color;
 import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.onebillion.onecourse.R;
 import org.onebillion.onecourse.utils.OBConfigManager;
 import org.onebillion.onecourse.utils.OBUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
@@ -27,16 +33,20 @@ public class OBSetupMenu_AttributionsView extends OBSectionController
     public void initScreen()
     {
         MainActivity.mainActivity.setContentView(R.layout.layout_web);
-        final WebView webView = (WebView) MainActivity.mainActivity.findViewById(R.id.webview);
-        webView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                return true;
-            }
-        });
-        webView.setLongClickable(false);
+        TextView textView = (TextView) MainActivity.mainActivity.findViewById(R.id.textview);
+        textView.setTextColor(Color.BLACK);
+        //
+//        final WebView webView = (WebView) MainActivity.mainActivity.findViewById(R.id.webview);
+//        webView.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                return true;
+//            }
+//        });
+//        webView.setLongClickable(false);
         //
         String fileName = "attributions.html";
+//        String fileName = "ATTRIBUTIONS.md";
         String url = null;
         //
         for (File externalDir : OBConfigManager.sharedManager.getExternalAssetsSearchPaths())
@@ -63,7 +73,11 @@ public class OBSetupMenu_AttributionsView extends OBSectionController
         //
         if (url != null)
         {
-            webView.loadUrl("file://" + url);
+            String filePath = url;
+            String contentHTML = readFileUsingBufferedReader(filePath);
+            Spanned content = Html.fromHtml(contentHTML);
+            textView.setText(content);
+//            webView.loadUrl("file://" + url);
         }
         //
         Button backButton = (Button) MainActivity.mainActivity.findViewById(R.id.back_button);
@@ -76,7 +90,25 @@ public class OBSetupMenu_AttributionsView extends OBSectionController
                 MainActivity.mainViewController.pushViewControllerWithNameConfig("OBSetupMenu", "", false, false, "menu", true);
             }
         });
+    }
 
+    private static String readFileUsingBufferedReader(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        {
+
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null)
+            {
+                contentBuilder.append(sCurrentLine).append("\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
     }
 
     public void prepare()
